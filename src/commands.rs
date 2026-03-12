9943f508412e265c924fd9a9180bf90355bac062
twigform <twigscoolemail@gmail.com>
2026-03-12 19:51:56 -0400

    add shutdown command

--- src/commands.rs
+++ src/commands.rs
@@ -6,3 +6,9 @@ pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say(response).await?;
     Ok(())
 }
+
+#[poise::command(prefix_command, owners_only)]
+pub async fn shutdown(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("ok bye :(").await?;
+    std::process::exit(0);
+}
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![commands::ping()],
+            commands: vec![commands::ping(), commands::shutdown()],
 
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some(">".into()),
